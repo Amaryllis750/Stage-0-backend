c3c128cdbe7a2694ecf70b60f4e215c785009fff
Okoronkwo Daniel <danielchukwuemela751@gmail.com>
2025-01-29 10:49:28 +0100

    Fixed the Date Parsing error

--- server.js
+++ server.js
@@ -3,18 +3,24 @@ const express = require('express');
 const cors = require('cors');
 
 const app = express();
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 8010;
 
 // setup the basic middleware
 app.use(express.json());    // parse json data from http requests
 app.use(express.urlencoded({ extended: false }));     // recognizes incoming requests as strings or arrays
 app.use(cors());    // add the cors to enable cors for all domains
 
+// function to get the current date and format it...
+const getCurrentDate = () => {
+   const now = new Date();
+   return now.toISOString().split('.')[0] + 'Z';
+}
+
 // setup the only route in the API
 app.get('/', async (req, res) => {
     const email = "danielchukwuemela751@gmail.com";
-    const github_url = "https://github.com/Amaryllis750/Stage-0-backend.git"
-    const current_datetime = (new Date()).toISOString();
+    const github_url = "https://github.com/Amaryllis750/Stage-0-backend.git";
+    const current_datetime = getCurrentDate();
 
     return res.status(200).json({ email, current_datetime, github_url });
 });
