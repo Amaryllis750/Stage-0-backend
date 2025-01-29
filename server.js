8024ebd401843cb3560fae5340bc3b18cd665e95
Okoronkwo Daniel <danielchukwuemela751@gmail.com>
2025-01-29 09:37:16 +0100

    Added CORS functionality to the API

--- server.js
+++ server.js
@@ -1,20 +1,25 @@
-    const express = require('express');
-    const app = express();
-    const PORT = 8010;
+// require packages
+const express = require('express');
+const cors = require('cors');
 
-    // setup the basic middleware
-    app.use(express.json());    // parse json data from http requests
-    app.use(express.urlencoded({extended: false}));     // recognizes incoming requests as strings or arrays
+const app = express();
+const PORT = 8010;
 
-    app.get('/', async(req, res)=> {
-        const email = "danielchukwuemela751@gmail.com";
-        const github_url = "https://github.com/Amaryllis750/Stage-0-backend.git"
-        const current_datetime = (new Date()).toISOString();
+// setup the basic middleware
+app.use(express.json());    // parse json data from http requests
+app.use(express.urlencoded({ extended: false }));     // recognizes incoming requests as strings or arrays
+app.use(cors());    // add the cors to enable cors for all domains
 
-        return res.status(200).json({email, current_datetime, github_url});
-    });
+// setup the only route in the API
+app.get('/', async (req, res) => {
+    const email = "danielchukwuemela751@gmail.com";
+    const github_url = "https://github.com/Amaryllis750/Stage-0-backend.git"
+    const current_datetime = (new Date()).toISOString();
+
+    return res.status(200).json({ email, current_datetime, github_url });
+});
 
 
-    app.listen(PORT, () => {
-        console.log("Server is listening on port", PORT)
-    });
+app.listen(PORT, () => {
+    console.log("Server is listening on port", PORT)
+});
